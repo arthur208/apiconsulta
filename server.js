efa53a0d3bf2598830ec36ac65a1ac39de4b27e0
arthur208 <arthurduartezacarias@gmail.com>
2024-09-26 20:33:53 -0300

    testes 7

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-const port = 80;
+const port = 3000;
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
